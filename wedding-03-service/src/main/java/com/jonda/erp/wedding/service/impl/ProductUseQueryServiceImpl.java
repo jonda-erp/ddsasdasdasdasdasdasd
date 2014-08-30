6fb2a35a824ac60464bdc079cab1e4b1b3ce6a6d
fengxb <fengxb.hunter@gmail.com>
2014-08-30 16:32:00 +0800

    道具使用

---
+++ wedding-03-service/src/main/java/com/jonda/erp/wedding/service/impl/ProductUseQueryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jonda.erp.wedding.service.impl;
+
+import com.jonda.common.dao.PageQueryDao;
+import com.jonda.common.dto.Page;
+import com.jonda.erp.wedding.domain.wedding.order.ProductUse;
+import com.jonda.erp.wedding.dto.order.OrderQueryParam;
+import com.jonda.erp.wedding.dto.order.OrderQueryResult;
+import com.jonda.erp.wedding.dto.product.ProductUseParam;
+import com.jonda.erp.wedding.service.OrderQueryService;
+import com.jonda.erp.wedding.service.ProductUseQueryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by rejoady on 2014/7/18.
+ */
+@Transactional
+@Service("productUseQueryService")
+public class ProductUseQueryServiceImpl implements ProductUseQueryService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductUseQueryService.class);
+
+    @Resource
+    private PageQueryDao pageQueryDao;
+
+    @Override
+    public Page<ProductUseParam> queryProductUse(ProductUseParam param) {
+        return pageQueryDao.query("com.jonda.erp.wedding.dao.product.ProductUseQueryDao.queryProductUsePage", param);
+    }
+}
