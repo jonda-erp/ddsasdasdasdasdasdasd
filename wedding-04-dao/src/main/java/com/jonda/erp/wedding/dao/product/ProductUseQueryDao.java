a420cca5c884764c81fb624086373c6946f79838
fengxb <fengxb.hunter@gmail.com>
2014-08-30 16:30:21 +0800

    道具使用DAO

---
+++ wedding-04-dao/src/main/java/com/jonda/erp/wedding/dao/product/ProductUseQueryDao.java
@@ -0,0 +1,12 @@
+package com.jonda.erp.wedding.dao.product;
+
+
+import com.jonda.common.dao.MyBatisRepository;
+
+/**
+ * Created by rejoady on 2014/7/20.
+ */
+@MyBatisRepository
+public interface ProductUseQueryDao {
+
+}
